f5d2d024d9e23b96ffec19c3fe0c8e06af9074cc
CuriousExpert24 <karthikbaga04@gmail.com>
2020-07-07 12:10:28 -0500

    Updates

---
+++ precache-manifest.a14e5c18c87e513e3fe7c7e6eeaa42a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58899563ba5c5128767e837b3ba71b7",
+    "url": "/covidhome/index.html"
+  },
+  {
+    "revision": "be65c6472f05141ffc37",
+    "url": "/covidhome/static/css/main.be1d3fd7.chunk.css"
+  },
+  {
+    "revision": "5b92b37f8afb8094dbbe",
+    "url": "/covidhome/static/js/2.1a780d85.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidhome/static/js/2.1a780d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be65c6472f05141ffc37",
+    "url": "/covidhome/static/js/main.ee36b440.chunk.js"
+  },
+  {
+    "revision": "7fa5be8a43189f3df39b",
+    "url": "/covidhome/static/js/runtime-main.5eaa0a6b.js"
+  }
+]);
